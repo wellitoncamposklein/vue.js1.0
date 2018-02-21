5044c58eb97a6e8cc4b9479656c1480e8c1be626
Welliton Klein <welliton.fokushima@gmail.com>
2018-02-21 12:40:25 -0300

    Criando primeiro webcomponent

--- app.js
+++ app.js
@@ -7,45 +7,101 @@ Vue.filter('doneLabel',function (value) {
 });
 
 Vue.filter('statusGeneral',function (value) {
-   if (value === false){
-       return 'Nenhuma conta cadastrada';
-   }
-   if (!value){
-       return 'Nenhuma conta a pagar';
-   }else{
-       return 'Existem '+value+' contas a serem pagas';
-   }
+    if (value === false){
+        return 'Nenhuma conta cadastrada';
+    }
+    if (!value){
+        return 'Nenhuma conta a pagar';
+    }else{
+        return 'Existem '+value+' contas a serem pagas';
+    }
 });
 
-var app = new Vue({
-    el: "#app",
-    data:{
-        test: '',
-        title: "Contas a Pagar",
-        menus: [
-            {id: 0,name:"Listar Contas"},
-            {id: 1,name:"Criar Conta"}
-        ],
-        activedView: 0,
-        formType:'insert',
-        bille: {
-            date_due: '',
-            name:'',
-            value: 0,
-            done: 1
-        },
-        names:[
-            'Conta de Luz',
-            'Conta de água',
-            'Conta de telefone',
-            'Internet',
-            'Gasolina'
-        ],
-        bills: [
-            {date_due: '20/08/2017', name:'Conta de luz',value: '70.99', done:1},
-            {date_due: '10/08/2017', name:'Conta de agua',value: '95.99', done:0},
-            {date_due: '10/08/2017', name:'Internet',value: '200.99', done:0}
-        ]
+var appComponent = Vue.extend({
+    template:`
+        <h3>{{ title }}</h3>
+        <h5 :class="{'gray': status === false, 'green': status === 0, 'red': status > 0}">
+            {{ status | statusGeneral}}
+        </h5>
+        <nav>
+            <ul>
+                <li v-for="menu in menus">
+                    <a href="#" @click.prevent="showView(menu.id)">{{menu.name}}</a>
+                </li>
+            </ul>
+        </nav>
+        <div v-if="activedView == 0">
+            <table border="1" cellpadding="10">
+                <thead>
+                <tr>
+                    <th>Vencimento</th>
+                    <th>Nome</th>
+                    <th>Valor</th>
+                    <th>Paga?</th>
+                    <th>Acoes</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="bill in bills">
+                    <td>{{bill.date_due}}</td>
+                    <td>{{bill.name}}</td>
+                    <td>{{bill.value | currency 'R$ ' 2}}</td>
+                    <td class="minha-classe" :class="{'nao-pago': bill.done === 0,'pago': bill.done === 1}">
+                        {{bill.done | doneLabel}}
+                    </td>
+                    <td>
+                        <a href="#" @click.prevent="loadbille(bill)">Editar</a> |
+                        <a href="#" @click.prevent="deletebille(index)">Excluir</a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div v-if="activedView == 1">
+            <form name="form" @submit.prevent="submit">
+                <label>Vencimento:</label>
+                <input type="text" v-model="bille.date_due"/><br/><br/>
+                <label>Nome:</label>
+                <select v-model="bille.name">
+                    <option v-for="o in names" v-model="o">{{ o }}</option>
+                </select><br><br>
+                <label>Valor:</label>
+                <input type="text" v-model="bille.value"/><br><br>
+                <label>Conta paga?</label>
+                <input type="checkbox" v-model="bille.done"/><br><br>
+                <input type="button" @click="submit" value="Enviar"/>
+            </form>
+        </div>
+    `,
+    data: function(){
+        return {
+            test: '',
+            title: "Contas a Pagar",
+            menus: [
+                {id: 0,name:"Listar Contas"},
+                {id: 1,name:"Criar Conta"}
+            ],
+            activedView: 0,
+            formType:'insert',
+            bille: {
+                date_due: '',
+                name:'',
+                value: 0,
+                done: 1
+            },
+            names:[
+                'Conta de Luz',
+                'Conta de água',
+                'Conta de telefone',
+                'Internet',
+                'Gasolina'
+            ],
+            bills: [
+                {date_due: '20/08/2017', name:'Conta de luz',value: '70.99', done:1},
+                {date_due: '10/08/2017', name:'Conta de agua',value: '95.99', done:0},
+                {date_due: '10/08/2017', name:'Internet',value: '200.99', done:0}
+            ]
+        };
     },
     computed:{
         status: function () {
@@ -93,4 +149,10 @@ var app = new Vue({
             }
         }
     }
+});
+
+Vue.component('app-component',appComponent);
+
+var app = new Vue({
+    el: "#app"
 });
