395762a75daedb874cee8dd143167d1a95cf5041
Welliton Klein <welliton.fokushima@gmail.com>
2018-03-08 16:45:23 -0300

    Menu de navegação

--- dist/bill.component.js
+++ dist/bill.component.js
@@ -1,7 +1,7 @@
 "use strict";
 
 window.billComponent = Vue.extend({
-    template: "\n        <nav>\n            <ul>\n                <li v-for=\"menu in menus\">\n                    <a v-link=\"{name: menu.routeName}\">{{menu.name}}</a>\n                </li>\n            </ul>\n        </nav>        \n        <router-view></router-view>\n    ",
+    template: "\n        <div class=\"navbar-fixed\">\n            <nav>\n                <div class=\"nav-wrapper container\">               \n                    <a href=\"#\" class=\"brand-logo right\">Fokushima Contas</a>\n                    <ul id=\"nav-mobile\" class=\"left\">\n                        <li v-for=\"menu in menus\">\n                            <a v-link=\"{name: menu.routeName}\">{{menu.name}}</a>\n                        </li>\n                    </ul>                   \n                </div>\n            </nav>\n        </div>        \n        <router-view></router-view>\n    ",
     data: function data() {
         return {
             menus: [
--- src/bill.component.js
+++ src/bill.component.js
@@ -1,12 +1,17 @@
 window.billComponent = Vue.extend({
     template: `
-        <nav>
-            <ul>
-                <li v-for="menu in menus">
-                    <a v-link="{name: menu.routeName}">{{menu.name}}</a>
-                </li>
-            </ul>
-        </nav>        
+        <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper container">               
+                    <a href="#" class="brand-logo right">Fokushima Contas</a>
+                    <ul id="nav-mobile" class="left">
+                        <li v-for="menu in menus">
+                            <a v-link="{name: menu.routeName}">{{menu.name}}</a>
+                        </li>
+                    </ul>                   
+                </div>
+            </nav>
+        </div>        
         <router-view></router-view>
     `,
     data() {
