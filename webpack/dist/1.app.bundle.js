a574f9119557ba7f512884eabe8c436327799b56
Welliton Klein <welliton.fokushima@gmail.com>
2018-03-14 17:15:18 -0300

    webpack.config.js

---
+++ webpack/dist/1.app.bundle.js
@@ -0,0 +1,20 @@
+webpackJsonp([1],[
+/* 0 */,
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_RESULT__ = function () {
+	    var clients = [
+	        {name: 'Cliente 1'},
+	        {name: 'Cliente 2'},
+	        {name: 'Cliente 3'},
+	        {name: 'Cliente 4'},
+	        {name: 'Cliente 5'},
+	    ];
+	    return clients;
+	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+
+	// module.exports = clients;
+
+/***/ }
+]);
